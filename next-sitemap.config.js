93a64e958eeb067e3a3e84b99c023b3217da0fe4
snjnpark <hello@snjnpark.kr>
2024-11-08 08:39:15 +0000

    [edit] sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: 'https://ttwittwi.vercel.app', // .게시하는 site의 url
-    generateRobotsTxt: false, // robots.txt generate 여부 (자동생성 여부)
-    sitemapSize: 7000, // sitemap별 최대 크기 (최대 크기가 넘어갈 경우 복수개의 sitemap으로 분리됨)
-    changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
-    priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
-}
+    siteUrl: 'https://ttwittwi.vercel.app', // 게시하는 site의 url
+    generateRobotsTxt: true, // robots.txt 자동생성 여부 (true로 변경)
+    sitemapSize: 7000, // sitemap별 최대 크기
+    changefreq: 'daily', // 페이지 주소 변경 빈도
+    priority: 1, // 페이지 주소 우선순위
+    exclude: ['/search', '/categories', '/tags', '/authors'], // 제외할 페이지 경로
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: '*', disallow: ['/search', '/categories', '/tags', '/authors'] }, // disallow 설정
+        ],
+    },
+};
